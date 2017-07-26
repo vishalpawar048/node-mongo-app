e141efd4bb1f2f4b03e34558e506aaf38a5b9286
vishalpawar048 <vishalpawar048@gmail.com>
2017-07-26 14:00:14 +0530

    heroku mongodb

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos',(req,res)=>{
         res.status(400).send(e);
     })
 });
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 
 app.get('/todos/:id',(req,res)=>{
     var id=req.params.id;
