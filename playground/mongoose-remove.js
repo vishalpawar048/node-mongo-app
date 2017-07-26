027ae1ebf8f714a173009f9cb3d0dc3afa69e648
vishalpawar048 <vishalpawar048@gmail.com>
2017-07-26 19:53:19 +0530

    delete route added

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,5 @@
+const mongoose=require('mongoose');
+
+const {ObjectId}=require('mongodb');
+var {mongoose}=require('./db/mongoose');
+var{Todo}=require('./models/todo');
--- server/server.js
+++ server/server.js
@@ -43,6 +43,23 @@ app.get('/todos/:id',(req,res)=>{
     
 });
 
+app.delete('/todos/:id',(req,res)=>{
+    var id=req.params.id;
+    if(!ObjectId.isValid(id)){
+         return  res.status(404).send();
+    }
+         Todo.findByIdAndRemove(id).then((doc)=>{
+             if(!doc){
+                 return res.status(404).send();
+             }
+        res.send({doc});
+}).catch((e)=>{
+     res.status(400).send();
+})
+});
+
+
+
 app.listen(port,()=>{
     console.log(`App started on ${port}`);
 })
